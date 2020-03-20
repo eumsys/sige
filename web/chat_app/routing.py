3607fe0f363a244cff970f22f6ac1a3df027749e
Roldan <roldan096@hotmail.com>
2020-03-19 18:00:53 -0600

    chat_02

--- web/chat_app/routing.py
+++ web/chat_app/routing.py
@@ -2,9 +2,9 @@ from channels.routing import route
 from chat_app import consumers
 
 channel_routing = [
-	route('websocket.connect', 		consumers.ws_add,			path=r'^/sucursales/$'),
-	route('websocket.receive', 		consumers.ws_message,		path=r'^/sucursales/$'),
-	route('websocket.disconnect',	consumers.ws_disconnect, 	path=r'^/sucursales/$'),
+	#route('websocket.connect', 		consumers.ws_add,			path=r'^/sucursales/$'),
+	#route('websocket.receive', 		consumers.ws_message,		path=r'^/sucursales/$'),
+	#route('websocket.disconnect',	consumers.ws_disconnect, 	path=r'^/sucursales/$'),
 	route('websocket.connect', 		consumers.ws_add,			path=r'^/chat/$'),
 	route('websocket.receive', 		consumers.ws_message,		path=r'^/chat/$'),
 	route('websocket.disconnect',	consumers.ws_disconnect, 	path=r'^/chat/$'),
